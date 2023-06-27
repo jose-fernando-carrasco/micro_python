4ead30ff39d8cd1b41d26feba7993d180aa51cf8
Fernando Oña <fernandocarrasc590@gmail.com>
2023-06-27 01:09:27 -0400

    db creado

--- app.py
+++ app.py
@@ -2,33 +2,75 @@ from flask import Flask, request, jsonify
 from pyzbar.pyzbar import decode
 import cv2
 import numpy as np
+import mariadb
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'microqr'
+
+def connect_to_database():
+    return mariadb.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        database=app.config['MYSQL_DB']
+    )
 
 @app.route('/scan-qr', methods=['POST'])
 def scan_qr():
-    # Verifica si se ha enviado una imagen en la solicitud
     if 'image' not in request.files:
         return jsonify({'error': 'No se ha proporcionado ninguna imagen'}), 400
 
-    # Lee la imagen desde la solicitud
     image = request.files['image'].read()
-
-    # Convierte la imagen en un arreglo NumPy
-    nparr = np.fromstring(image, np.uint8)
-
-    # Decodifica los códigos QR en la imagen
+    nparr = np.frombuffer(image, np.uint8)
     decoded_objects = decode(cv2.imdecode(nparr, cv2.IMREAD_COLOR))
+    
+    qr_code = decoded_objects[0].data.decode('utf-8')
 
-    # Extrae la información de los códigos QR decodificados
-    results = []
-    for obj in decoded_objects:
-        data = obj.data.decode('utf-8')
-        result = {'data': data, 'type': obj.type}
-        results.append(result)
+    
+    try:
+        conn = connect_to_database()
+        cursor = conn.cursor()
 
-    # Devuelve los resultados en formato JSON
-    return jsonify({'results': results})
+    
+        cursor.execute("SELECT * FROM producto WHERE cod = ?", (qr_code,))
+        result = cursor.fetchone()
+        if result:
+            id_xd = result[0]
+        else:
+            return jsonify({'error': 'No se encontró ningún producto con el código QR proporcionado'}), 404
+
+        print("respuesta id")
+        print(id_xd)
+   
+        cursor.execute("SELECT producto.id, producto.nombre, sucursal.name, sucursal.ubicacion, producto.caducidad FROM producto "
+                    "JOIN sucursal_producto ON producto.id = sucursal_producto.producto_id "
+                    "JOIN sucursal ON sucursal_producto.sucursal_cod = sucursal.cod "
+                    "WHERE producto.id = ?", (id_xd,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+
+        print("Aswer")
+        print(result)
+
+        if result:
+            product = {
+                'id': result[0],
+                'nombre_producto': result[1],
+                'nombre_sucursal': result[2],
+                'ubicacion_sucursal': result[3],
+                'caducidad': result[4]
+            }
+            return jsonify({'product': product})
+        else:
+            return jsonify({'error': 'No se encontró ningún producto con el código QR proporcionado'}), 404
+
+    except mariadb.Error as e:
+        return jsonify({'error': f'Error de conexión a MariaDB: {e}'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
